da9300b3f168a4f749632286b5d812b7417feca8
Mark Goldring <mark.goldring@panoramicdata.com>
2020-08-17 09:58:41 +0100

    Added PDToolbarSeparator

---
+++ PanoramicData.Blazor/PDToolbarSeparator.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PanoramicData.Blazor
+{
+	public partial class PDToolbarSeparator : IToolbarItem
+    {
+		/// <summary>
+		/// Gets or sets the tooltip for the toolbar item.
+		/// </summary>
+		[Parameter] public string ToolTip { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Gets or sets whether the toolbar item is visible.
+		/// </summary>
+		[Parameter] public bool IsVisible { get; set; } = true;
+
+		/// <summary>
+		/// Gets or sets whether the toolbar item is enabled.
+		/// </summary>
+		[Parameter] public bool IsEnabled { get; set; } = true;
+
+		/// <summary>
+		/// Gets or sets whether the toolbar item is positioned further to the right of the previous toolbar item.
+		/// </summary>
+		[Parameter] public bool ShiftRight { get; set; } = false;
+	}
+}
