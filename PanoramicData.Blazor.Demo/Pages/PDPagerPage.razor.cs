8252eed66835eae4a3c9d6b6eea2740201a23911
Mark Goldring <mark.goldring@panoramicdata.com>
2020-11-03 12:55:48 +0000

    Added demo source view to Pager page

--- PanoramicData.Blazor.Demo/Pages/PDPagerPage.razor.cs
+++ PanoramicData.Blazor.Demo/Pages/PDPagerPage.razor.cs
@@ -1,14 +1,18 @@
-﻿namespace PanoramicData.Blazor.Demo.Pages
+﻿using Microsoft.AspNetCore.Components;
+using PanoramicData.Blazor.Demo.Data;
+
+namespace PanoramicData.Blazor.Demo.Pages
 {
 	public partial class PDPagerPage
     {
+		[CascadingParameter] protected EventManager? EventManager { get; set; }
+
 		private int CurrentPage { get; set; } = 1;
 		private int PageCount { get; set; } = 5;
-		private string Message { get; set; } = string.Empty;
 
-		private void PageChangeHandler(int page)
+		private void OnPageChanged(int page)
 		{
-			Message = $"Current page changed to {page}";
+			EventManager?.Add(new Event("PageChanged", new EventArgument("Page", page)));
 			CurrentPage = page;
 		}
 	}
