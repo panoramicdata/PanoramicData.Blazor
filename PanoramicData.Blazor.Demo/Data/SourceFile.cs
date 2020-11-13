38d0ba582dd9cedac17c08e0c58939fd57376463
Mark Goldring <mark.goldring@panoramicdata.com>
2020-11-13 12:01:23 +0000

    Modifed DemoSourceView to always include link to _Host.cshtml source

---
+++ PanoramicData.Blazor.Demo/Data/SourceFile.cs
@@ -0,0 +1,9 @@
+﻿namespace PanoramicData.Blazor.Demo.Data
+{
+	public class SourceFile
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Url { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+	}
+}
--- PanoramicData.Blazor.Demo/Shared/DemoSourceView.razor.cs
+++ PanoramicData.Blazor.Demo/Shared/DemoSourceView.razor.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
+using PanoramicData.Blazor.Demo.Data;
 
 namespace PanoramicData.Blazor.Demo.Shared
 {
@@ -11,7 +12,7 @@ namespace PanoramicData.Blazor.Demo.Shared
 		private string ActiveTab { get; set; } = "Demo";
 		private const string SourceBaseUrl = "https://raw.githubusercontent.com/panoramicdata/PanoramicData.Blazor/main/PanoramicData.Blazor.Demo";
 		private HttpClient _httpClient = new HttpClient();
-		private Dictionary<string, string> _sourceFiles = new Dictionary<string, string>();
+		private Dictionary<string, SourceFile> _sourceFiles = new Dictionary<string, SourceFile>();
 		private string _activeSourceFile = string.Empty;
 
 		/// <summary>
@@ -30,13 +31,29 @@ namespace PanoramicData.Blazor.Demo.Shared
 			{
 				foreach (var sourceFile in SourceFiles.Split(',', StringSplitOptions.RemoveEmptyEntries))
 				{
-					var filename = sourceFile.Trim();
-					var content = await LoadSourceAsync(filename).ConfigureAwait(true);
-					_sourceFiles.Add(filename, content);
-					if(string.IsNullOrWhiteSpace(_activeSourceFile))
+					var trimmedSourceFile = sourceFile.Trim();
+					var name = trimmedSourceFile[(trimmedSourceFile.LastIndexOf('/') + 1)..];
+					_sourceFiles.Add(name, new SourceFile
 					{
-						_activeSourceFile = filename;
+						Name = name,
+						Url = GetUrl(trimmedSourceFile)
+					});
+					if (string.IsNullOrWhiteSpace(_activeSourceFile))
+					{
+						_activeSourceFile = name;
 					}
+				}
+				// add _Host.cshtml to every page
+				_sourceFiles.Add("_Host.cshtml", new SourceFile
+				{
+					Name = "_Host.cshtml",
+					Url = "https://raw.githubusercontent.com/panoramicdata/PanoramicData.Blazor/main/PanoramicData.Blazor.Web/Pages/_Host.cshtml"
+				});
+				// load first source file
+				if (!string.IsNullOrWhiteSpace(_activeSourceFile))
+				{
+					var entry = _sourceFiles[_activeSourceFile];
+					entry.Content = await _httpClient.GetStringAsync(entry.Url).ConfigureAwait(true);
 				}
 			}
 		}
@@ -47,22 +64,39 @@ namespace PanoramicData.Blazor.Demo.Shared
 			{
 				if(_sourceFiles.ContainsKey(_activeSourceFile))
 				{
-					return _sourceFiles[_activeSourceFile];
+					return _sourceFiles[_activeSourceFile].Content;
 				}
 				return "";
 			}
 		}
 
-		private async Task<string> LoadSourceAsync(string relativeUrl)
+		public string GetUrl(string url)
+		{
+			return url.StartsWith("http", StringComparison.OrdinalIgnoreCase) ? url : $"{SourceBaseUrl}/{url}";
+		}
+
+		private async Task<string> LoadSourceAsync(string url)
 		{
 			try
 			{
-				var url = $"{SourceBaseUrl}/{relativeUrl}";
-				return await _httpClient.GetStringAsync(url).ConfigureAwait(true);
+				return await _httpClient.GetStringAsync(GetUrl(url)).ConfigureAwait(true);
 			}
 			catch(Exception ex)
 			{
 				return $"Failed to load source: {ex.Message}";
+			}
+		}
+
+		private async Task OnFileClick(string name)
+		{
+			if (_sourceFiles.ContainsKey(name))
+			{
+				var sourceFile = _sourceFiles[name];
+				if(string.IsNullOrWhiteSpace(sourceFile.Content))
+				{
+					sourceFile.Content = await LoadSourceAsync(sourceFile.Url).ConfigureAwait(true);
+				}
+				_activeSourceFile = name;
 			}
 		}
 	}
