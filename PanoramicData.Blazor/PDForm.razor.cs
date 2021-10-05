6475a49d56f532c5d064ff8599e14ca098e96451
Mark Goldring <mark.goldring@panoramicdata.com>
2021-10-05 10:30:31 +0100

    PDFrom: Modified ValidateFieldAsync method to set object instance in validation context to original item with changes applied. This is required when comparing two properties etc.

--- PanoramicData.Blazor/PDForm.razor.cs
+++ PanoramicData.Blazor/PDForm.razor.cs
@@ -572,8 +572,8 @@ namespace PanoramicData.Blazor
 						object? typedValue = (value ?? GetFieldValue(field, true))?.Cast(propInfo.PropertyType);
 
 						// run standard data annotation validation
-						var results = new List<ValidationResult>();
-						var context = new ValidationContext(Item)
+						var results = new List<ValidationResult>();						
+						var context = new ValidationContext(GetItemWithUpdates())
 						{
 							MemberName = memberInfo.Name
 						};
