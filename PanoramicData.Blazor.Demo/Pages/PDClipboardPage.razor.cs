8c0ecd6a99bfcb9fb3e07ed9bd8bb1dc541d8128
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2022-09-26 18:13:58 +0100

    Added basic clipboard component

---
+++ PanoramicData.Blazor.Demo/Pages/PDClipboardPage.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace PanoramicData.Blazor.Demo.Pages
+{
+	public partial class PDClipboardPage
+	{
+		private readonly string _text = Guid.NewGuid().ToString();
+		private readonly string _text2 = Guid.NewGuid().ToString();
+		private readonly string _text3 = Guid.NewGuid().ToString();
+	}
+}
---
+++ PanoramicData.Blazor/PDClipboard.razor.cs
@@ -0,0 +1,44 @@
+﻿namespace PanoramicData.Blazor
+{
+	public partial class PDClipboard
+	{
+		[Inject] IJSRuntime JSRuntime { get; set; } = null!;
+
+		/// <summary>
+		/// Text
+		/// </summary>
+		[Parameter] public string Text { get; set; } = string.Empty;
+
+		/// <summary>
+		/// CSS class to apply when the copy button is ready to be clicked
+		/// </summary>
+		[Parameter] public string ReadyToCopyCssClass { get; set; } = "far fa-copy";
+
+		/// <summary>
+		/// CSS class to apply when the text has been copied
+		/// </summary>
+		[Parameter] public string TextCopiedCssClass { get; set; } = "fas fa-check";
+
+		/// <summary>
+		/// General CSS Class to apply
+		/// </summary>
+		[Parameter] public string CssClass { get; set; } = string.Empty;
+
+		private string _buttonClass = string.Empty;
+
+		protected override void OnInitialized()
+		{
+			_buttonClass = ReadyToCopyCssClass;
+		}
+
+		private async Task CopyTextToClipboardAsync()
+		{
+			await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", Text);
+			_buttonClass = TextCopiedCssClass;
+			StateHasChanged();
+			await Task.Delay(1000);
+			_buttonClass = ReadyToCopyCssClass;
+			StateHasChanged();
+		}
+	}
+}
