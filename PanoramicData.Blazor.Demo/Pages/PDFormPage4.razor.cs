a3206d70288082b40888fc556e30b31d5499294a
Mark Goldring <mark.goldring@panoramicdata.com>
2023-03-23 10:29:46 +0000

    PDFormBody - Now displays all helpers for grouped fields.

--- PanoramicData.Blazor.Demo/Pages/PDFormPage4.razor.cs
+++ PanoramicData.Blazor.Demo/Pages/PDFormPage4.razor.cs
@@ -4,6 +4,11 @@ namespace PanoramicData.Blazor.Demo.Pages;
 
 public partial class PDFormPage4
 {
+	private FormFieldHelper<RegisterModel> _docxHelper;
+	private FormFieldHelper<RegisterModel> _htmlHelper;
+	private FormFieldHelper<RegisterModel> _pdfHelper;
+	private FormFieldHelper<RegisterModel> _xlsxHelper;
+
 	private readonly DelegatedDataProvider<RegisterModel> _dataProvider = new()
 	{
 		CreateAsync = (model, cancellationToken) =>
@@ -27,5 +32,45 @@ public partial class PDFormPage4
 
 	[AllowNull]
 	private PDForm<RegisterModel> _registerForm;
+
+	public PDFormPage4()
+	{
+		_docxHelper = new FormFieldHelper<RegisterModel>
+		{
+			ClickAsync = async (x) =>
+			{
+				await _registerForm!.SetFieldValueAsync(x, !_registerForm.GetFieldValue<bool>(nameof(RegisterModel.ReportFormatDocx), true));
+				return new FormFieldResult();
+			},
+			IconCssClass = "fas -fa-fw fa-toggle-on c-docx"
+		};
+		_htmlHelper = new FormFieldHelper<RegisterModel>
+		{
+			ClickAsync = async (x) =>
+			{
+				await _registerForm!.SetFieldValueAsync(x, !_registerForm.GetFieldValue<bool>(nameof(RegisterModel.ReportFormatHtml), true));
+				return new FormFieldResult();
+			},
+			IconCssClass = "fas -fa-fw fa-toggle-on c-html"
+		};
+		_pdfHelper = new FormFieldHelper<RegisterModel>
+		{
+			ClickAsync = async (x) =>
+			{
+				await _registerForm!.SetFieldValueAsync(x, !_registerForm.GetFieldValue<bool>(nameof(RegisterModel.ReportFormatPdf), true));
+				return new FormFieldResult();
+			},
+			IconCssClass = "fas -fa-fw fa-toggle-on c-pdf"
+		};
+		_xlsxHelper = new FormFieldHelper<RegisterModel>
+		{
+			ClickAsync = async (x) =>
+			{
+				await _registerForm!.SetFieldValueAsync(x, !_registerForm.GetFieldValue<bool>(nameof(RegisterModel.ReportFormatXlsx), true));
+				return new FormFieldResult();
+			},
+			IconCssClass = "fas -fa-fw fa-toggle-on c-xlsx"
+		};
+	}
 
 }
