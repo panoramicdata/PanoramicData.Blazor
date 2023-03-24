98b1f6f0744651d41c0bb4cb13b122cf675c4c3d
Mark Goldring <mark.goldring@panoramicdata.com>
2023-03-24 12:18:51 +0000

    Added Single and Multiple selection

--- PanoramicData.Blazor.Demo/Pages/PDCardDeckPage.razor.cs
+++ PanoramicData.Blazor.Demo/Pages/PDCardDeckPage.razor.cs
@@ -2,12 +2,22 @@
 
 public partial class PDCardDeckPage
 {
-	private List<Todo> _todoList = new();
+	private List<Todo> _todoList1 = new();
+	private List<Todo> _todoList2 = new();
 
 	protected override void OnInitialized()
 	{
 		// generate some todos
-		_todoList.AddRange(new[] {
+		_todoList1.AddRange(new[] {
+			new Todo("Do weekly shop"),
+			new Todo("Record Antiques Roadshow"),
+			new Todo("Put up shelf"),
+			new Todo("Get a haircut"),
+			new Todo("Walk the dog")
+		});
+
+		// generate some todos
+		_todoList2.AddRange(new[] {
 			new Todo("Do weekly shop"),
 			new Todo("Record Antiques Roadshow"),
 			new Todo("Put up shelf"),
--- PanoramicData.Blazor/PDCardDeck.razor.cs
+++ PanoramicData.Blazor/PDCardDeck.razor.cs
@@ -3,6 +3,7 @@
 public partial class PDCardDeck<TItem>
 {
 	private static int _sequence;
+	private readonly List<TItem> _selection = new();
 
 	[Inject]
 	public IJSRuntime JSRuntime { get; set; } = null!;
@@ -16,6 +17,58 @@ public partial class PDCardDeck<TItem>
 
 	[Parameter]
 	public IEnumerable<TItem> Items { get; set; } = Array.Empty<TItem>();
+
+	[Parameter]
+	public bool MultipleSelection { get; set; }
+
+	public IEnumerable<TItem> GetSelection() => _selection;
+
+	private Task OnItemMouseUpAsync(TItem item, MouseEventArgs args)
+	{
+		UpdateSelection(item, args.CtrlKey, args.ShiftKey);
+		return Task.CompletedTask;
+	}
+
+	private void UpdateSelection(TItem item, bool ctrl, bool shift)
+	{
+		if (IsEnabled)
+		{
+			if (MultipleSelection)
+			{
+				if (ctrl) // add/remove single
+				{
+					if (_selection.Contains(item))
+					{
+						_selection.Remove(item);
+					}
+					else
+					{
+						_selection.Add(item);
+					}
+				}
+				else if (shift && _selection.Count > 0) // add range
+				{
+					var list = Items.ToList();
+					var sIdx = list.IndexOf(_selection.First());
+					var eIdx = list.IndexOf(item);
+					var s = Math.Min(sIdx, eIdx);
+					var e = Math.Max(sIdx, eIdx);
+					_selection.Clear();
+					_selection.AddRange(list.GetRange(s, e - s + 1));
+				}
+				else
+				{
+					_selection.Clear();
+					_selection.Add(item);
+				}
+			}
+			else
+			{
+				_selection.Clear();
+				_selection.Add(item);
+			}
+		}
+	}
 
 	private string SizeCssClass => Size switch
 	{
