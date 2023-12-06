400380493984c45f705922432cf60ed279dea8b8
Mark Goldring <markgoldring@hotmail.com>
2023-12-06 12:26:51 +0000

    BC-19 Update to dotnet 8 - Updated Target Framework and libraries.

--- PanoramicData.Blazor/Exceptions/PDTreeException.cs
+++ PanoramicData.Blazor/Exceptions/PDTreeException.cs
@@ -30,15 +30,4 @@ public class PDTreeException : PDBlazorException
 	public PDTreeException(string message, Exception innerException) : base(message, innerException)
 	{
 	}
-
-	/// <summary>
-	/// Initializes a new instance of the PDTreeException class with serialized data.
-	/// </summary>
-	/// <param name="info">The System.Runtime.Serialization.SerializationInfo that holds the serialized object
-	/// data about the exception being thrown.</param>
-	/// <param name="context"> The System.Runtime.Serialization.StreamingContext that contains contextual
-	/// information about the source or destination.</param>
-	protected PDTreeException(SerializationInfo info, StreamingContext context) : base(info, context)
-	{
-	}
 }
