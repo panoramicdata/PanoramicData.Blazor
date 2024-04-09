1c2ec3c5e3e9a913a15e315f297fa884ac577ff1
Mark Goldring <markgoldring@hotmail.com>
2024-04-09 11:51:28 +0100

    BC-26 - WIP

---
+++ PanoramicData.Blazor.Demo/Data/DemoPreviewProvider.cs
@@ -0,0 +1,13 @@
+﻿using PanoramicData.Blazor.PreviewProviders;
+
+namespace PanoramicData.Blazor.Demo.Data;
+
+public class DemoPreviewProvider : FileExplorerPreviewProvider
+{
+	protected override async Task<byte[]> DownloadContentAsync(FileExplorerItem item)
+	{
+		await Task.Delay(5000);
+
+		return await base.DownloadContentAsync(item);
+	}
+}
