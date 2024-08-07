b12c1b7cc01b450629cb54b55f2743cc5d5452a6
Mark Goldring <markgoldring@hotmail.com>
2024-08-07 12:10:37 +0100

    Monaco demo page - Added initial completion for NCalc (System.Math functions).

--- PanoramicData.Blazor.Demo/Pages/PDMonaco.razor.js
+++ PanoramicData.Blazor.Demo/Pages/PDMonaco.razor.js
@@ -68,11 +68,14 @@ export function configureMonaco() {
 		// Register language
 		monaco.languages.register({ id: 'ncalc' });
 
-		// Register a tokens provider for the languages
+		// Register a tokens provider
 		monaco.languages.setMonarchTokensProvider('ncalc', getNCalcLanguage());
+
+		// Register a completion provider
+		monaco.languages.registerCompletionItemProvider("ncalc", {
+			provideCompletionItems: getNCalcCompletions
+		});
 	}
-
-
 }
 
 function getNCalcLanguage() {
@@ -160,6 +163,348 @@ function getNCalcLanguage() {
 			],
 		}
 	}
+}
+
+function getNCalcCompletions(model, position) {
+
+	// find out if we are completing a property in the 'dependencies' object.
+	var textUntilPosition = model.getValueInRange({
+		startLineNumber: 1,
+		startColumn: 1,
+		endLineNumber: position.lineNumber,
+		endColumn: position.column,
+	});
+
+	//var match = textUntilPosition.match(
+	//	/"dependencies"\s*:\s*\{\s*("[^"]*"\s*:\s*"[^"]*"\s*,\s*)*([^"]*)?$/
+	//);
+	//if (!match) {
+	//	return { suggestions: [] };
+	//}
+	var word = model.getWordUntilPosition(position);
+	var range = {
+		startLineNumber: position.lineNumber,
+		endLineNumber: position.lineNumber,
+		startColumn: word.startColumn,
+		endColumn: word.endColumn,
+	};
+
+	return {
+		suggestions: [
+			{
+				label: 'Abs',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the absolute value of a number.",
+				insertText: 'Abs(',
+				range: range,
+			},
+			{
+				label: 'Acos',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose cosine is the specified number.",
+				insertText: 'Acos(',
+				range: range,
+			},
+			{
+				label: 'Acosh(',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose hyperbolic cosine is the specified number.",
+				insertText: 'Acosh(',
+				range: range,
+			},
+			{
+				label: 'Asin',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose sine is the specified number.",
+				insertText: 'Asin(',
+				range: range,
+			},
+			{
+				label: 'Asinh(',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose hyperbolic sine is the specified number.",
+				insertText: 'Asinh(',
+				range: range,
+			},
+			{
+				label: 'Atan',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose tangent is the specified number.",
+				insertText: 'Atan(',
+				range: range,
+			},
+			{
+				label: 'Atan2',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose tangent is the quotient of two specified numbers.",
+				insertText: 'Atan2(',
+				range: range,
+			},
+			{
+				label: 'Atan2',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the angle whose hyperbolic tangent is the specified number.",
+				insertText: 'Atanh(',
+				range: range,
+			},
+			{
+				label: 'BigMul',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Produces the full product of two numbers.",
+				insertText: 'BigMul(',
+				range: range,
+			},
+			{
+				label: 'BitDecrement',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the largest value that compares less than a specified value.",
+				insertText: 'BitDecrement(',
+				range: range,
+			},
+			{
+				label: 'BitIncrement',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the smallest value that compares greater than a specified value.",
+				insertText: 'BitIncrement(',
+				range: range,
+			},
+			{
+				label: 'Cbrt',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the cube root of a specified number.",
+				insertText: 'Cbrt(',
+				range: range,
+			},
+			{
+				label: 'Ceiling',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the smallest integral value that is greater than or equal to the specified number.",
+				insertText: 'Ceiling(',
+				range: range,
+			},
+			{
+				label: 'Clamp',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns value clamped to the inclusive range of min and max.",
+				insertText: 'Clamp(',
+				range: range,
+			},
+			{
+				label: 'CopySign',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns a value with the magnitude of x and the sign of y.",
+				insertText: 'CopySign(',
+				range: range,
+			},
+			{
+				label: 'Cos',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the cosine of the specified angle.",
+				insertText: 'Cos(',
+				range: range,
+			},
+			{
+				label: 'Cosh',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the hyperbolic cosine of the specified angle.",
+				insertText: 'Cosh(',
+				range: range,
+			},
+			{
+				label: 'DivRem',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Produces the quotient and the remainder of two numbers.",
+				insertText: 'DivRem(',
+				range: range,
+			},
+			{
+				label: 'Exp',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns e raised to the specified power.",
+				insertText: 'Exp(',
+				range: range,
+			},
+			{
+				label: 'Floor',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the largest integral value less than or equal to the specified decimal number.",
+				insertText: 'Floor(',
+				range: range,
+			},
+			{
+				label: 'FusedMultiplyAdd',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns (x * y) + z, rounded as one ternary operation.",
+				insertText: 'FusedMultiplyAdd(',
+				range: range,
+			},
+			{
+				label: 'IEEERemainder',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the remainder resulting from the division of a specified number by another specified number.",
+				insertText: 'IEEERemainder(',
+				range: range,
+			},
+			{
+				label: 'ILogB',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the base 2 integer logarithm of a specified number.",
+				insertText: 'ILogB(',
+				range: range,
+			},
+			{
+				label: 'Log',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the natural (base e) logarithm of a specified number.",
+				insertText: 'Log(',
+				range: range,
+			},
+			{
+				label: 'Log10',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the base 10 logarithm of a specified number.",
+				insertText: 'Log10(',
+				range: range,
+			},
+			{
+				label: 'Log2',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the base 2 logarithm of a specified number.",
+				insertText: 'Log2(',
+				range: range,
+			},
+			{
+				label: 'Max',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the larger of two numbers.",
+				insertText: 'Max(',
+				range: range,
+			},
+			{
+				label: 'MaxMagnitude',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the larger magnitude of two numbers.",
+				insertText: 'MaxMagnitude(',
+				range: range,
+			},
+			{
+				label: 'MaxMagnitude',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the larger magnitude of two double- precision floating - point numbers.",
+				insertText: 'MaxMagnitude(',
+				range: range,
+			},
+			{
+				label: 'Min',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the smaller of two 8-bit unsigned integers.",
+				insertText: 'Min(',
+				range: range,
+			},
+			{
+				label: 'MinMagnitude',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the smaller magnitude of two double- precision floating - point numbers.",
+				insertText: 'MinMagnitude(',
+				range: range,
+			},
+			{
+				label: 'Pow',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns a specified number raised to the specified power.",
+				insertText: 'Pow(',
+				range: range,
+			},
+			{
+				label: 'ReciprocalEstimate',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns an estimate of the reciprocal of a specified number.",
+				insertText: 'ReciprocalEstimate(',
+				range: range,
+			},
+			{
+				label: 'ReciprocalSqrtEstimate',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns an estimate of the reciprocal square root of a specified number.",
+				insertText: 'ReciprocalSqrtEstimate(',
+				range: range,
+			},
+			{
+				label: 'Round',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Rounds a decimal value to the nearest integral value, and rounds midpoint values to the nearest even number.",
+				insertText: 'Round(',
+				range: range,
+			},
+			{
+				label: 'ScaleB',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns x * 2^n computed efficiently.",
+				insertText: 'ScaleB(',
+				range: range,
+			},
+			{
+				label: 'Sign',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns an integer that indicates the sign of a number.",
+				insertText: 'Sign(',
+				range: range,
+			},
+			{
+				label: 'Sin',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the sine of the specified angle.",
+				insertText: 'Sin(',
+				range: range,
+			},
+			{
+				label: 'SinCos',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the sine and cosine of the specified angle.",
+				insertText: 'SinCos(',
+				range: range,
+			},
+			{
+				label: 'Sinh',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the hyperbolic sine of the specified angle.",
+				insertText: 'Sinh(',
+				range: range,
+			},
+			{
+				label: 'Sqrt',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the square root of a specified number.",
+				insertText: 'Sqrt(',
+				range: range,
+			},
+			{
+				label: 'Tan',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the tangent of the specified angle.",
+				insertText: 'Tan(',
+				range: range,
+			},
+			{
+				label: 'Tanh',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Returns the hyperbolic tangent of the specified angle.",
+				insertText: 'Tanh(',
+				range: range,
+			},
+			{
+				label: 'Truncate',
+				kind: monaco.languages.CompletionItemKind.Function,
+				documentation: "Calculates the integral part of a specified number.",
+				insertText: 'Truncate(',
+				range: range,
+			}
+		],
+	};
+}
+
+function getMathCompletions(model, position) {
+
 }
 
 function getRMScriptLanguage() {
