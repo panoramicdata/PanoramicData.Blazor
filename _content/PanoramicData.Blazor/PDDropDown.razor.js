94220b6b4a77bd22b61c7b235796ede28c150ecb
TheCakeMonster <46025270+TheCakeMonster@users.noreply.github.com>
2024-10-09 09:40:33 +0000

    Deploying to gh-pages from main @ 32c650496ebaee3859f8c1b5d9806f43ba1f1df0 🚀

---
+++ _content/PanoramicData.Blazor/PDDropDown.razor.js
@@ -0,0 +1,25 @@
+﻿export function initialize(id, ref, opt) {
+	var el = document.getElementById(id);
+	if (el) {
+
+		el.parentElement.addEventListener("keypress", function (ev) {
+			if (ev.keyCode === 13 && ref) {
+				ref.invokeMethodAsync("OnKeyPressed", 13);
+			}
+		});
+
+		el.addEventListener("shown.bs.dropdown", function () {
+			if (ref) {
+				ref.invokeMethodAsync("OnDropDownShown");
+			}
+		});
+
+		el.addEventListener("hidden.bs.dropdown", function () {
+			if (ref) {
+				ref.invokeMethodAsync("OnDropDownHidden");
+			}
+		});
+
+		return new bootstrap.Dropdown(el, opt);
+	}
+}
