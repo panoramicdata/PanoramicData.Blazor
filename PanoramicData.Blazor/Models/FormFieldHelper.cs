73e2705591fd98d69f08058f6e108b53464d209f
Mark Goldring <markgoldring@hotmail.com>
2024-12-11 17:12:55 +0000

    PDForm - Fields can now display helper Icons and Tooltips based on the item under edit.

--- PanoramicData.Blazor/Models/FormFieldHelper.cs
+++ PanoramicData.Blazor/Models/FormFieldHelper.cs
@@ -22,7 +22,18 @@ public class FormFieldHelper<TItem> where TItem : class
 	public string IconCssClass { get; set; } = string.Empty;
 
 	/// <summary>
+	/// Gets or sets the icon for the button but can vary based on the item under edit.
+	/// </summary>
+	public Func<TItem, string>? IconCssClass2 { get; set; }
+
+	/// <summary>
 	/// Gets or set a tooltip for the button.
 	/// </summary>
 	public string ToolTip { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Gets or set a tooltip for the button but can vary based on the item under edit.
+	/// </summary>
+	public Func<TItem, string>? ToolTip2 { get; set; }
+
 }
