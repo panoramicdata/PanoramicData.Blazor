883dbd687a3804abb798bbb6a8a5eb0f81dc55ad
MarkGoldring <66791190+MarkGoldring@users.noreply.github.com>
2024-12-19 12:31:10 +0000

    Deploying to gh-pages from main @ 90b552594a9ba7d6a9ee889f55df7c5c9c83239e 🚀

---
+++ _content/PanoramicData.Blazor/PDContextMenu.razor.js
@@ -0,0 +1,50 @@
+﻿var menuEl = null;
+var popper = null;
+
+export function hasPopperJs() {
+	return typeof Popper !== "undefined";
+}
+
+export function showMenu(menuId, x, y) {
+	menuEl = document.getElementById(menuId);
+	if (menuEl) {
+		var reference = {
+			getBoundingClientRect() {
+				return {
+					width: 0,
+					height: 0,
+					top: y,
+					bottom: y,
+					left: x,
+					right: x
+				};
+			}
+		};
+		var options = {
+			placement: 'bottom-start',
+			positionFixed: true
+		};
+		menuEl.classList.add("show");
+		popper = Popper.createPopper(reference, menuEl, options); // this is popper v2.4.4 syntax
+		document.addEventListener("mousedown", documentMouseDown);
+	}
+}
+
+export function hideMenu() {
+	if (menuEl) {
+		menuEl.classList.remove("show");
+		document.removeEventListener("mousedown", documentMouseDown);
+		if (popper) {
+			popper.destroy();
+		}
+	}
+}
+
+export function documentMouseDown(event) {
+	if (menuEl && popper) {
+		let isClickInside = menuEl.contains(event.target);
+		if (!isClickInside) {
+			hideMenu();
+		}
+	}
+}
