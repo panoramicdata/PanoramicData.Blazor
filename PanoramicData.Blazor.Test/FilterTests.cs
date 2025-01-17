d49e321e98fedacf2f8ef6dc0d748f3c9947aa59
David Bond <david.bond@panoramicdata.com>
2025-01-17 00:26:56 +0000

    Improved DateTime Filter

---
+++ PanoramicData.Blazor.Test/FilterTests.cs
@@ -0,0 +1,18 @@
+﻿using PanoramicData.Blazor.Models;
+using Shouldly;
+
+namespace PanoramicData.Blazor.Test;
+
+public class FilterTests
+{
+	[Fact]
+	public void Test1()
+	{
+		var filter = Filter.ParseMany("claimedAt:>\"15/08/2023 21:26:07 +01:00\"").ToList();
+		filter.Count.ShouldBe(1);
+		var firstFilter = filter.First();
+		firstFilter.Key.ShouldBe("claimedAt");
+		firstFilter.FilterType.ShouldBe(FilterTypes.GreaterThan);
+		firstFilter.Value.ShouldBe("\"2023-08-15T20:26:07Z\"");
+	}
+}
