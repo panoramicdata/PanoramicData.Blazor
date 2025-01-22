dd538a31b78e78d566448ba577343886249a3dd0
Roland Banks <roland.banks@panoramicdata.com>
2025-01-22 12:45:05 +0700

    Updated nuget packages.

--- PanoramicData.Blazor.Test/FilterTests.cs
+++ PanoramicData.Blazor.Test/FilterTests.cs
@@ -10,7 +10,7 @@ public class FilterTests
 	{
 		var filter = Filter.ParseMany("claimedAt:>\"15/08/2023 21:26:07 +01:00\"").ToList();
 		filter.Count.ShouldBe(1);
-		var firstFilter = filter.First();
+		var firstFilter = filter[0];
 		firstFilter.Key.ShouldBe("claimedAt");
 		firstFilter.FilterType.ShouldBe(FilterTypes.GreaterThan);
 		firstFilter.Value.ShouldBe("\"2023-08-15T20:26:07Z\"");
