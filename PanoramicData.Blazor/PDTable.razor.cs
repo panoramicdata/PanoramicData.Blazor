915de3994b8b6bdb8b5d7233332cc659801606f5
john-odlin <114656551+john-odlin@users.noreply.github.com>
2025-03-20 16:22:32 +0700

    BC-58: Dates in Filter Distinct

--- PanoramicData.Blazor/PDTable.razor.cs
+++ PanoramicData.Blazor/PDTable.razor.cs
@@ -1040,9 +1040,9 @@ public partial class PDTable<TItem> :
 
 		// Cast to string
 		//return objectValues.Select(x => Filter.Format(x, filter.UnspecifiedDateTimesAreUtc)).ToArray();
-		return objectValues.Select(x => string.IsNullOrEmpty(column.Format)
+		return [.. objectValues.Select(x => string.IsNullOrEmpty(column.Format)
 				? x.ToString() ?? string.Empty
-				: string.Format(CultureInfo.CurrentCulture, "{0:" + column.Format + "}", x)).ToArray();
+				: string.Format(CultureInfo.CurrentCulture, "{0:" + column.Format + "}", x)).Distinct()];
 	}
 
 	protected override void OnParametersSet()
