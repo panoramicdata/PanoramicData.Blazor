24b117b255f5d347ae9cde63e92d68f7cacd42e5
john-odlin <114656551+john-odlin@users.noreply.github.com>
2025-05-02 22:54:37 +0000

    Deploying to gh-pages from main @ 75b4e063c6a86d0cdabc943127589b272711b327 🚀

---
+++ _content/PanoramicData.Blazor/PDGlobalListener.razor.js
@@ -0,0 +1,59 @@
+﻿var globalListenerReference = null;
+var shortcutKeys = [];
+
+export function initialize(ref) {
+	globalListenerReference = ref;
+	window.addEventListener("keydown", onKeyDown);
+	window.addEventListener("keyup", onKeyUp);
+}
+
+export function dispose() {
+	window.removeEventListener("keydown", onKeyDown);
+	window.removeEventListener("keyup", onKeyUp);
+	globalListenerReference = null;
+}
+
+export function registerShortcutKeys(shortcuts) {
+	shortcutKeys = shortcuts || [];
+}
+
+export function isShortcutKeyMatch(keyInfo) {
+	var match = shortcutKeys.find((v) => v.altKey == keyInfo.altKey &&
+		v.ctrlKey == keyInfo.ctrlKey &&
+		v.shiftKey == keyInfo.shiftKey &&
+		(v.key.toLowerCase() == keyInfo.key.toLowerCase()) || (v.code.toLowerCase() == keyInfo.code.toLowerCase()));
+	return match ? true : false;
+}
+
+function onKeyDown(e) {
+	if (globalListenerReference) {
+		var keyInfo = getKeyArgs(e);
+		if (isShortcutKeyMatch(keyInfo)) {
+			e.stopPropagation();
+			e.preventDefault();
+		}
+		globalListenerReference.invokeMethodAsync("OnKeyDown", keyInfo);
+	}
+}
+
+function onKeyUp(e) {
+	if (globalListenerReference) {
+		var keyInfo = getKeyArgs(e);
+		if (isShortcutKeyMatch(keyInfo)) {
+			e.stopPropagation();
+			e.preventDefault();
+		}
+		globalListenerReference.invokeMethodAsync("OnKeyUp", keyInfo);
+	}
+}
+
+export function getKeyArgs(e) {
+	var obj = {};
+	obj.key = e.key;
+	obj.code = e.code;
+	obj.keyCode = e.keyCode;
+	obj.altKey = e.altKey;
+	obj.ctrlKey = e.ctrlKey;
+	obj.shiftKey = e.shiftKey;
+	return obj;
+}
