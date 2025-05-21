27bb9a9a7720bd66dba1a31f1a641af0492aefb4
john-odlin <114656551+john-odlin@users.noreply.github.com>
2025-05-21 13:49:01 +0000

    Deploying to gh-pages from main @ 60f0fd9e582934211efcfc3a2be6da78520d3f03 🚀

---
+++ _content/PanoramicData.Blazor/PDDropDown.razor.js
@@ -0,0 +1,36 @@
+﻿export function initialize(id, toggleId, dropdownId, ref, opt) {
+	var el = document.getElementById(toggleId);
+	if (ref && el) {
+
+		el.parentElement.addEventListener("keypress", function (ev) {
+			if (ev.keyCode === 13) {
+				ref.invokeMethodAsync("OnKeyPressed", 13);
+			}
+		});
+
+		el.addEventListener("shown.bs.dropdown", function () {
+			ref.invokeMethodAsync("OnDropDownShown");
+		});
+
+		el.addEventListener("hidden.bs.dropdown", function () {
+			ref.invokeMethodAsync("OnDropDownHidden");
+		});
+
+		el.addEventListener("mouseleave", function (ev) {
+			if (ev.relatedTarget.parentElement.id != id) {
+				ref.invokeMethodAsync("OnMouseLeave");
+			}
+		});
+
+		var dropdownEl = document.getElementById(dropdownId);
+		if (dropdownEl) {
+			dropdownEl.addEventListener("mouseleave", function (ev) {
+				if (!ev.relatedTarget || ev.relatedTarget.parentElement.id != id) {
+					ref.invokeMethodAsync("OnMouseLeave");
+				}
+			});
+		}
+
+		return new bootstrap.Dropdown(el, opt);
+	}
+}
