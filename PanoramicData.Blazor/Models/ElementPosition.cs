99e6e78e3b354ca04d5666866d79a13430be4639
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2025-07-01 16:26:01 +0100

    BC-7, Added GetHashCode method to ElementPosition

--- PanoramicData.Blazor/Models/ElementPosition.cs
+++ PanoramicData.Blazor/Models/ElementPosition.cs
@@ -1,5 +1,8 @@
 ﻿namespace PanoramicData.Blazor.Models
 {
+	/// <summary>
+	/// Represents the position of an element on the page.
+	/// </summary>
 	internal class ElementPosition
 	{
 		public double Top { get; set; }
@@ -12,7 +15,11 @@
 			{
 				return Top.Equals(other.Top) && Left.Equals(other.Left);
 			}
+
 			return false;
 		}
+
+		public override int GetHashCode()
+				=> (Top, Left).GetHashCode();
 	}
 }
